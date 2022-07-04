e9c42004c94e5df51374c244f4ef645c9903e5a7
wenzhiTeo <wenzhi@infin8co.com>
2022-07-04 17:29:50 +0800

    update apollo server settings

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-async function startApolloServer(server) {
   const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
-  console.log(`Sever is runing on http://localhost:${port}`);
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
 }
 
-startApolloServer(server);
+startApolloServer(typeDefs, resolvers);
