b56220afa49998fa4e0aef460e4c76cd6c33de9d
wenzhiTeo <99173738+wenzhiTeo@users.noreply.github.com>
2022-07-11 00:16:08 +0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://apollo-lift-off-server-wenzhi.herokuapp.com/",
+  uri: "https://apollo-lift-off-server-wenzhi.herokuapp.com",
   cache: new InMemoryCache(),
 });
 
